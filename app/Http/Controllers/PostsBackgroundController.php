9b3dc007212789217509c6d03b2a32ba101a3ce4
Le Truong Thinh <ltruongthinh2002@gmail.com>
2024-07-22 08:51:04 +0700

    #1 - updated create model controller faction file

---
+++ app/Http/Controllers/PostsBackgroundController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\posts_background;
+use App\Http\Requests\Storeposts_backgroundRequest;
+use App\Http\Requests\Updateposts_backgroundRequest;
+
+class PostsBackgroundController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Storeposts_backgroundRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(posts_background $posts_background)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(posts_background $posts_background)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Updateposts_backgroundRequest $request, posts_background $posts_background)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(posts_background $posts_background)
+    {
+        //
+    }
+}
